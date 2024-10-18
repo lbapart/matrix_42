aee3977a52fadf1eb03b35d9c7f51f97d0a5acbb
Артем Апенко <lbapart@gmail.com>
2024-10-18 14:49:40 +0200

    ex03

---
+++ ex03/Utils.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Matrix.hpp"
+#include "Vector.hpp"
+#include <stdexcept>
+
+#define RUNTIME_ERROR "Runtime error"
+
+namespace utils
+{
+    template<typename T>
+    T lerp(const T &first, const T &second, const double t)
+    {
+        if (t < 0 || t > 1)
+            throw std::runtime_error(RUNTIME_ERROR);
+        
+        double first_scalar = 1 - t;
+        double second_scalar = t;
+
+        try {
+            T result(first);
+
+            result *= first_scalar;
+            result += second_scalar * second;
+            return result;
+        } catch (const std::runtime_error& e){
+            throw e;
+        }
+    }
+}
